7212d9036c9e7354ff4c6f74253398bc27f0a7db
cloudonshore <samuel.walker9@gmail.com>
2019-05-14 14:31:41 -0400

    updates

--- src/reserves/redux/index.js
+++
@@ -1,5 +0,0 @@
-import middleware from './middleware'
-import reducers, { selectors, containers } from './reducers'
-import * as actions from './actions'
-
-export { middleware, reducers, selectors, containers, actions }
