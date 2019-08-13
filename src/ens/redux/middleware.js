703dd3f4e235ed5b679609777480259853a59f7c
Graham Perich <graham.perich@gmail.com>
2019-08-13 13:43:06 -0400

    fix ens integration

--- src/ens/redux/middleware.js
+++ src/ens/redux/middleware.js
@@ -1,33 +1,36 @@
-// import { getAddress, getName, setupENS } from '@ensdomains/ui'
-// import { setENSReady, gotENSLookupError, gotENSLookupSuccess } from './actions'
+import { getAddress, getName, setupENS } from '@ensdomains/ui'
+import { setENSReady, gotENSLookupError, gotENSLookupSuccess } from './actions'
+import { httpProvider } from '../../constants'
 
 // eslint-disable-next-line
 export default function ensMiddleware(store) {
-  // setupENS().then(() => store.dispatch(setENSReady()))
+  setupENS({ customProvider: httpProvider }).then(() => {
+    store.dispatch(setENSReady())
+  })
   return next => action => {
     switch (action.type) {
       case 'FIND_ADDRESS_BY_ENS_NAME':
-        // const { name } = action
-        // getAddress(name)
-        //   .then(address => {
-        //     store.dispatch(gotENSLookupSuccess(address, name))
-        //   })
-        //   .catch(e => {
-        //     store.dispatch(gotENSLookupError(e.message))
-        //   })
+        const { name } = action
+        getAddress(name)
+          .then(address => {
+            store.dispatch(gotENSLookupSuccess(address, name))
+          })
+          .catch(e => {
+            store.dispatch(gotENSLookupError(e.message))
+          })
         next(action)
         break
 
       case 'FIND_ENS_NAME_BY_ADDRESS':
-        // const { address } = action
-        // getName(address)
-        //   .then(ensName => {
-        //     store.dispatch(gotENSLookupSuccess(address, ensName))
-        //   })
-        //   .catch(e => {
-        //     store.dispatch(gotENSLookupError(e.message))
-        //   })
-        // next(action)
+        const { address } = action
+        getName(address)
+          .then(ensName => {
+            store.dispatch(gotENSLookupSuccess(address, ensName))
+          })
+          .catch(e => {
+            store.dispatch(gotENSLookupError(e.message))
+          })
+        next(action)
         break
       default:
     }
