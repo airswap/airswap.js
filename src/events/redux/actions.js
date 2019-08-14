eaf29b56f7308cf8fe24975f9d30bae5ee2f678c
cloudonshore <samuel.walker9@gmail.com>
2019-08-14 15:52:07 -0400

    migrated event tracking to ABI driven generative redux tooling

--- src/events/redux/actions.js
+++ src/events/redux/actions.js
@@ -1,24 +1,5 @@
-export const fetchHistoricalFillsByMakerAddress = makerAddress => ({
-  type: 'FETCH_HISTORICAL_FILLS_BY_MAKER_ADDRESS',
-  makerAddress,
-})
+import { trackSwapCancel, trackSwapSwap } from '../../swap/redux/eventTrackingActions'
 
-export const fetchHistoricalCancelsByMakerAddress = makerAddress => ({
-  type: 'FETCH_HISTORICAL_CANCELS_BY_MAKER_ADDRESS',
-  makerAddress,
-})
+export const fetchHistoricalSwapFillsByMakerAddress = makerWallet => trackSwapSwap({ makerWallet, fromBlock: 0 })
 
-export const fetchHistoricalFailuresByMakerAddress = makerAddress => ({
-  type: 'FETCH_HISTORICAL_FAILURES_BY_MAKER_ADDRESS',
-  makerAddress,
-})
-
-export const fetchHistoricalSwapFillsByMakerAddress = makerAddress => ({
-  type: 'FETCH_HISTORICAL_SWAP_FILLS_BY_MAKER_ADDRESS',
-  makerAddress,
-})
-
-export const fetchHistoricalSwapCancelsByMakerAddress = makerAddress => ({
-  type: 'FETCH_HISTORICAL_SWAP_CANCELS_BY_MAKER_ADDRESS',
-  makerAddress,
-})
+export const fetchHistoricalSwapCancelsByMakerAddress = makerWallet => trackSwapCancel({ makerWallet, fromBlock: 0 })
