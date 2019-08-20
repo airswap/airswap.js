3985a5f482f6dd204be297a934b32ad66358a3f8
cloudonshore <samuel.walker9@gmail.com>
2019-08-20 00:14:29 -0400

    fix unchecked provider

--- src/wallet/uncheckedJsonRpcSigner.js
+++ src/wallet/uncheckedJsonRpcSigner.js
@@ -1,6 +1,6 @@
 const ethers = require('ethers')
 const { httpProvider } = require('../constants')
-
+const { poll } = require('ethers/utils/web')
 // from: https://github.com/ethers-io/ethers.js/issues/340#issuecomment-447512944
 class UncheckedJsonRpcSigner extends ethers.Signer {
   constructor(signer) {
@@ -14,7 +14,22 @@ class UncheckedJsonRpcSigner extends ethers.Signer {
   }
 
   sendTransaction(transaction) {
-    return this.signer.sendUncheckedTransaction(transaction).then(hash => httpProvider.getTransaction(hash))
+    // return this.signer.sendUncheckedTransaction(transaction).then(hash => httpProvider.getTransaction(hash))
+
+    return this.signer.sendUncheckedTransaction(transaction).then(hash =>
+      poll(
+        () =>
+          httpProvider.getTransaction(hash).then(tx => {
+            if (tx === null) {
+              return undefined
+            }
+            return tx
+          }),
+        { fastRetry: 250 },
+      ).catch(error => {
+        throw error
+      }),
+    )
   }
 
   signMessage(message) {
