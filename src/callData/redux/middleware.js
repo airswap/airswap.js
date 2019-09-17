8d5b7a0891c3aedcbf5f0fa43cf551524950aa07
Shawn Kim <syjk.dev@gmail.com>
2019-09-17 14:27:27 -0400

    dispatch erc721 approval in calldata middleware

--- src/callData/redux/middleware.js
+++ src/callData/redux/middleware.js
@@ -1,5 +1,6 @@
 import { fetchSwapDelegateApprovals } from '../../swap/redux/contractFunctionActions'
 import { fetchERC20Allowance } from '../../erc20/redux/contractFunctionActions'
+import { fetchERC721GetApproved } from '../../erc721/redux/contractFunctionActions'
 
 export default function callData(store) {
   return next => action => {
@@ -18,14 +19,23 @@ export default function callData(store) {
         }
 
         if (eventName === 'approval') {
-          store.dispatch(
-            fetchERC20Allowance({
-              // addresses need to be lowercased, since their responses are matched using the input parameters in lookups
-              contractAddress: event.address.toLowerCase(),
-              owner: event.values.owner.toLowerCase(),
-              spender: event.values.spender.toLowerCase(),
-            }),
-          )
+          if (action.namespace === 'ERC721') {
+            store.dispatch(
+              fetchERC721GetApproved({
+                contractAddress: event.parameters.contractAddress,
+                tokenId: event.parameters.tokenId,
+              }),
+            )
+          } else {
+            store.dispatch(
+              fetchERC20Allowance({
+                // addresses need to be lowercased, since their responses are matched using the input parameters in lookups
+                contractAddress: event.address.toLowerCase(),
+                owner: event.values.owner.toLowerCase(),
+                spender: event.values.spender.toLowerCase(),
+              }),
+            )
+          }
         }
         break
       default:
