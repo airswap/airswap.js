5c4fb224b4507ce9ec9fb41bfdb8f1702375588c
cloudonshore <samuel.walker9@gmail.com>
2019-11-15 11:34:21 -0500

    added delegate creation

---
+++ src/delegateFactory/redux/actions.js
@@ -0,0 +1,7 @@
+import { getConnectedWalletAddress } from '../../wallet/redux/reducers'
+import { submitDelegateFactoryCreateDelegate } from './contractFunctionActions'
+
+export const createDelegateForConnectedWallet = () => (dispatch, getState) => {
+  const address = getConnectedWalletAddress(getState())
+  dispatch(submitDelegateFactoryCreateDelegate({ delegateContractOwner: address, delegateTradeWallet: address }))
+}
--- src/delegateFactory/redux/index.js
+++ src/delegateFactory/redux/index.js
@@ -1,9 +1,12 @@
-import middleware from './middleware'
+import defaultMiddleware from './middleware'
+import contractFunctionMiddleware from './contractFunctionMiddleware'
 import reducers from './reducers'
 import * as eventTrackingSelectors from './eventTrackingSelectors'
 
 const selectors = {
   ...eventTrackingSelectors,
 }
+
+const middleware = [defaultMiddleware, contractFunctionMiddleware]
 
 export { middleware, reducers, selectors }
