c537a114a3b5fc12f1c48201ff15235098e00441
cloudonshore <samuel.walker9@gmail.com>
2019-11-19 11:29:21 -0500

    added rule creation tracking

--- src/delegate/redux/actions.js
+++ src/delegate/redux/actions.js
@@ -1,6 +1,33 @@
 import { submitDelegateSetRule, submitDelegateSetRuleAndIntent } from './contractFunctionActions'
 import { getConnectedDelegateContract } from '../../delegateFactory/redux/selectors'
 import { submitSwapAuthorizeSender } from '../../swap/redux/contractFunctionActions'
+import { getContractPriceFromDisplayPrice } from '../utils' //eslint-disable-line
+
+// EXAMPLE ACTION: sell 25 AST at 0.005 AST/WETH
+export const exampleSetRuleAndIntent = () => dispatch =>
+  getContractPriceFromDisplayPrice({
+    senderToken: '0xcc1cbd4f67cceb7c001bd4adf98451237a193ff8',
+    signerToken: '0xc778417e063141139fce010982780140aa0cd5ab',
+    senderAmountDisplayValue: '25',
+    priceDisplayValue: '0.005',
+  }).then(rule =>
+    dispatch(
+      submitConnectedDelegateSetRuleAndIntent({
+        senderToken: '0xcc1cbd4f67cceb7c001bd4adf98451237a193ff8',
+        signerToken: '0xc778417e063141139fce010982780140aa0cd5ab',
+        rule,
+        amountToStake: '0',
+      }),
+    ),
+  )
+
+export const exampleSetRule = () => dispatch =>
+  getContractPriceFromDisplayPrice({
+    senderToken: '0xcc1cbd4f67cceb7c001bd4adf98451237a193ff8',
+    signerToken: '0xc778417e063141139fce010982780140aa0cd5ab',
+    senderAmountDisplayValue: '25',
+    priceDisplayValue: '0.005',
+  }).then(rule => dispatch(submitConnectedDelegateSetRule(rule)))
 
 export const submitConnectedDelegateSetRuleAndIntent = ({ senderToken, signerToken, rule, amountToStake }) => (
   dispatch,
--- src/delegate/redux/middleware.js
+++ src/delegate/redux/middleware.js
@@ -3,6 +3,11 @@ import { getConnectedDelegateContract } from '../../delegateFactory/redux/select
 import { trackSwapAuthorizeSender } from '../../swap/redux/eventTrackingActions'
 import { getConnectedWalletAddress } from '../../wallet/redux/reducers'
 import { fetchSwapSenderAuthorizations } from '../../swap/redux/contractFunctionActions'
+import { trackDelegateSetRule, trackDelegateUnsetRule } from './eventTrackingActions'
+import { fetchDelegateRules } from './contractFunctionActions'
+import { exampleSetRuleAndIntent } from './actions'
+import { DELEGATE_FACTORY_CONTRACT_DEPLOY_BLOCK } from '../../constants'
+import { getDelegateRules } from './callDataSelectors'
 
 async function waitForDelegateContract(store) {
   return store.dispatch(
@@ -15,24 +20,65 @@ async function waitForDelegateContract(store) {
 
 export default function delegateMiddleware(store) {
   return next => action => {
+    console.log(getDelegateRules(store.getState()))
     switch (action.type) {
       case 'REDUX_STORAGE_LOAD':
         next(action)
         waitForDelegateContract(store).then(() => {
+          // as soon as a delegate contract is found for the connected address, invoke the following listeners:
           const state = store.getState()
           const walletAddress = getConnectedWalletAddress(state)
-          const contractAddress = getConnectedDelegateContract(state)
+          const delegateAddress = getConnectedDelegateContract(state)
+
+          store.dispatch(exampleSetRuleAndIntent())
+
+          // listen to swap sender authorizations for the delegate and update the state accordingly
           store.dispatch(
             trackSwapAuthorizeSender({
               authorizerAddress: walletAddress,
-              authorizedSender: contractAddress,
+              authorizedSender: delegateAddress,
+              fromBlock: DELEGATE_FACTORY_CONTRACT_DEPLOY_BLOCK,
               callback: () =>
                 store.dispatch(
                   fetchSwapSenderAuthorizations({
                     authorizerAddress: walletAddress,
-                    authorizedSender: contractAddress,
+                    authorizedSender: delegateAddress,
                   }),
                 ),
+            }),
+          )
+          // listen to rule creation on the delegate and update the contract accordingly
+          store.dispatch(
+            trackDelegateSetRule({
+              ruleOwner: walletAddress,
+              fromBlock: DELEGATE_FACTORY_CONTRACT_DEPLOY_BLOCK,
+              callback: events => {
+                events.map(({ values: { senderToken, signerToken } }) => {
+                  store.dispatch(
+                    fetchDelegateRules({
+                      senderToken,
+                      signerToken,
+                      contractAddress: delegateAddress,
+                    }),
+                  )
+                })
+              },
+            }),
+          )
+          // listen to rule delegation on the delegate and update the contract accordingly
+          store.dispatch(
+            trackDelegateUnsetRule({
+              ruleOwner: walletAddress,
+              callback: events =>
+                events.map(({ values: { senderToken, signerToken } }) => {
+                  store.dispatch(
+                    fetchDelegateRules({
+                      senderToken,
+                      signerToken,
+                      contractAddress: delegateAddress,
+                    }),
+                  )
+                }),
             }),
           )
         })
