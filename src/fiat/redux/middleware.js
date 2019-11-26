9c20d4aa28c6e3d957a72b01b23c802ae1754bbf
cloudonshore <samuel.walker9@gmail.com>
2019-11-26 17:46:17 -0500

    fixed for trader

--- src/fiat/redux/middleware.js
+++ src/fiat/redux/middleware.js
@@ -1,12 +1,15 @@
 import { makeMiddlewareHTTPFn } from '../../utils/redux/templates/http'
 import { fetchTokenPrice } from '../index'
+import { IS_INSTANT } from '../../constants'
 
 function fetchEthPrice() {
   return fetchTokenPrice('ETH')
 }
 
 export default function gasMiddleware(store) {
-  makeMiddlewareHTTPFn(fetchEthPrice, 'ethPrices', store, { increment: 60 * 1000 })
+  if (IS_INSTANT) {
+    makeMiddlewareHTTPFn(fetchEthPrice, 'ethPrices', store, { increment: 60 * 1000 })
+  }
   return next => action => {
     switch (action.type) {
       default:
