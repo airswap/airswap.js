f4a91d6539d089d394283ceaa1305ee34c6c89df
cloudonshore <samuel.walker9@gmail.com>
2019-12-02 15:08:47 -0500

    removed console.log

--- src/indexer/redux/middleware.js
+++ src/indexer/redux/middleware.js
@@ -1,11 +1,9 @@
 import { IS_INSTANT, INDEXER_CONTRACT_DEPLOY_BLOCK } from '../../constants'
 import { trackIndexerCreateIndex } from './eventTrackingActions'
 import { fetchIndexerIndexes } from './contractFunctionActions'
-import { getLocatorIntents } from './selectors'
 
 export default function indexerMiddleware(store) {
   return next => action => {
-    console.log(getLocatorIntents(store.getState()))
     switch (action.type) {
       case 'REDUX_STORAGE_LOAD':
         if (IS_INSTANT) {
