77a07cb1b79e6431376dbb8ea441ff9ddfdd90d9
Ethan Wessel <ethan.wessel@fluidity.io>
2020-01-07 14:41:34 -0500

    tests with values

--- src/indexer/indexer.test.js
+++ src/indexer/indexer.test.js
@@ -1,13 +1,14 @@
+const assert = require('assert')
 const contractFunctions = require('./contractFunctions')
 
 describe('Indexer Tests', async () => {
   it('Test getIndexerIsOwner()', async () => {
     const response = await contractFunctions.getIndexerIsOwner()
-    console.log(response)
+    assert.equal(response, true)
   })
 
   it('Test getIndexerOnwer()', async () => {
     const response = await contractFunctions.getIndexerOwner()
-    console.log(response)
+    assert.equal(response, '0x7F18BB4Dd92CF2404C54CBa1A9BE4A1153bdb078')
   })
 })
