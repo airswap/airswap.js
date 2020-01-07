7255782d1a52a3674f90d4c98ce300681cad2187
Ethan Wessel <ethan.wessel@fluidity.io>
2020-01-07 15:03:41 -0500

    tests - checkpoint

--- src/indexer/indexer.test.js
+++ src/indexer/indexer.test.js
@@ -1,7 +1,18 @@
 const assert = require('assert')
 const contractFunctions = require('./contractFunctions')
+const constants = require('../constants')
 
 describe('Indexer Tests', async () => {
+  it('Test getIndexerIndexes()', async () => {
+    const response = await contractFunctions.getIndexerIndexes(
+      constants.DAI_CONTRACT_ADDRESS,
+      constants.WETH_CONTRACT_ADDRESS,
+      constants.PROTOCOL_1,
+    )
+    // assert equal(response, '')
+    console.log(response)
+  })
+
   it('Test getIndexerIsOwner()', async () => {
     const response = await contractFunctions.getIndexerIsOwner()
     assert.equal(response, true)
