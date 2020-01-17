8d0c82999e14ed68bd7e356fcf4604474c2e7fae
cloudonshore <samuel.walker9@gmail.com>
2020-01-17 14:43:59 -0500

    added options for excluding alchemy websockets

--- get_alchemy.js
+++ get_alchemy.js
@@ -1,3 +1,3 @@
 require('dotenv').config()
 
-console.log(`https://eth-mainnet.alchemyapi.io/jsonrpc/${process.env.ALCHEMY_API_KEY}`)
+console.log(`https://eth-mainnet.alchemyapi.io/jsonrpc/1e8iSwEIqstMQtW1133tjieia8pkQ4a8`)
