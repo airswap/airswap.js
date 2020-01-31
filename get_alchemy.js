e13f4160329265083c87f98535fcb8899934a74c
cloudonshore <samuel.walker9@gmail.com>
2020-01-31 11:37:10 -0500

    reverted get_alchemy.js

--- get_alchemy.js
+++ get_alchemy.js
@@ -1,5 +1,3 @@
 require('dotenv').config()
 
-console.log(
-  `https://wild-long-flower.quiknode.io/2a86ed60-9ae4-415c-bcd7-161c467158d9/wBRA65iQEoSKqRVcnTlEmMfVYvIgew2X9QEdXxVLgZrKlKTi2wnydX3Q68KOsaeJ7ZiFsyoqxV-MQID2bAbVPw==/`,
-)
+console.log(`https://eth-mainnet.alchemyapi.io/jsonrpc/${process.env.ALCHEMY_API_KEY}`)
