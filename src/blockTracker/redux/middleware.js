25a7993dd4c2db6087f7fe08c253ae4ef5d18b12
cloudonshore <samuel.walker9@gmail.com>
2020-02-14 12:48:41 -0500

    add comment

--- src/blockTracker/redux/middleware.js
+++ src/blockTracker/redux/middleware.js
@@ -1,6 +1,8 @@
 // import blockTracker from '../index'
 // import { gotLatestBlock } from './actions'
 
+// TODO: removing blocktracking for now to get alchemy callrate down
+// in the future will need to make alchemy websocket optional again
 export default function blockTrackerMiddleware() {
   // blockTracker.onBlock(block => store.dispatch(gotLatestBlock(block)))
   return next => action => {
