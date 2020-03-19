e09b02535b06b6f64656d1ac66882d1084f34045
cloudonshore <samuel.walker9@gmail.com>
2020-03-19 16:06:53 -0400

    fixed bug in compliance service address lookup

--- src/erc1155/redux/selectors.js
+++ src/erc1155/redux/selectors.js
@@ -1,3 +1,4 @@
+import _ from 'lodash'
 import { createSelector } from 'reselect'
 import { getERC1155GetComplianceService } from './callDataSelectors'
 import { getAllinfraIsWhitelisted } from '../../allinfra/redux/callDataSelectors'
@@ -10,10 +11,15 @@ export const getComplianceServiceByAddress = createSelector(getERC1155GetComplia
   return Object.fromEntries(formattedResponses)
 })
 
-export const getAllinfraWhitelist = createSelector(getAllinfraIsWhitelisted, responses =>
-  responses.map(({ response, parameters: { contractAddress, account } }) => ({
-    whitelisted: response,
-    walletAddress: account,
-    erc1155Address: contractAddress,
-  })),
+export const getAllinfraWhitelist = createSelector(
+  getComplianceServiceByAddress,
+  getAllinfraIsWhitelisted,
+  (complianceServiceByAddress, responses) => {
+    const erc1155lookup = _.invert(complianceServiceByAddress)
+    return responses.map(({ response, parameters: { contractAddress, account } }) => ({
+      whitelisted: response,
+      walletAddress: account,
+      erc1155Address: erc1155lookup[contractAddress],
+    }))
+  },
 )
