b37c8359640a4e19d1cb00d8c9ea0f52b5438cb1
cloudonshore <samuel.walker9@gmail.com>
2020-04-14 10:51:41 -0400

    fixed fiat middleware

--- src/fiat/redux/middleware.js
+++ src/fiat/redux/middleware.js
@@ -1,12 +1,4 @@
-// import { makeMiddlewareHTTPFn } from '../../utils/redux/templates/http'
-// import { fetchTokenPrice } from '../index'
-// import { IS_INSTANT } from '../../constants'
-
-// function fetchEthPrice() {
-//   return fetchTokenPrice('ETH')
-// }
-
-export default function gasMiddleware() {
+export default function fiatMiddleware() {
   return next => action => {
     switch (action.type) {
       default:
