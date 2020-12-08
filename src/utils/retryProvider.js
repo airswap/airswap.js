f0cbe1ef16490d1f489d6c3c20ee066f91b7c42a
Don Mosites <don.mosites@fluidity.io>
2020-12-08 13:16:09 +0100

    Replace RetryProvider.send for Firefox fix

--- src/utils/retryProvider.js
+++ src/utils/retryProvider.js
@@ -1,27 +1,36 @@
 const { JsonRpcProvider } = require('ethers/providers')
 const { poll } = require('ethers/utils/web')
+const axios = require('axios')
 
-const ATTEMPTS = 5
+const MAX_ATTEMPTS = 5
 
 class RetryProvider extends JsonRpcProvider {
   constructor(url, network) {
     super(url, network)
-    this.attempts = ATTEMPTS
+    this.url = url
+    this.id = 1
   }
 
-  perform(method, params) {
+  send(method, params) {
     let attempts = 0
     return poll(() => {
       attempts++
-      return super.perform(method, params).then(
-        result => result,
-        error => {
-          if ((error.statusCode !== 0 && error.statusCode !== 429) || attempts >= this.attempts) {
-            return Promise.reject(error)
-          }
-          return Promise.resolve(undefined)
-        },
-      )
+      return axios
+        .post(this.url, {
+          method,
+          params,
+          id: this.id++,
+          jsonrpc: '2.0',
+        })
+        .then(
+          response => Promise.resolve(response.data.result),
+          response => {
+            if ((response.statusCode !== 0 && response.statusCode !== 429) || attempts >= MAX_ATTEMPTS) {
+              return Promise.reject(response)
+            }
+            return Promise.resolve(undefined)
+          },
+        )
     })
   }
 }
