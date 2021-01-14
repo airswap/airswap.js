f9aaac6fc86f0c03fee6407acc250be2973587cf
Don Mosites <don.mosites@fluidity.io>
2021-01-14 11:45:28 +0100

    Generate correct URL for etherescan (mainnet)

--- src/utils/etherscan.js
+++ src/utils/etherscan.js
@@ -1,8 +1,7 @@
-const _ = require('lodash')
 const { NETWORK_NAME } = require('../constants')
 
 function makeEtherscanURL(hash, type) {
-  return `https://${_.isUndefined(NETWORK_NAME) ? '' : `${NETWORK_NAME}.`}etherscan.io/${type}/${hash}`
+  return `https://${NETWORK_NAME === 'mainnet' ? '' : `${NETWORK_NAME}.`}etherscan.io/${type}/${hash}`
 }
 
 function makeEtherscanAddress(address) {
